95ee6a13de0990151bc4fc5b5cb22723968474d6
ElJakes <jidejacobs1@gmail.com>
2023-07-23 16:47:28 +0100

    write code to generate random meme url from memeData.js

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,6 +1,15 @@
 import React from 'react'
-
+import memesData from '../memData'
+// console.log(memesData)
 const Meme = () => {
+
+    const getMemeImage = () => {
+    const memeArray = memesData.data.memes
+    const randomNumber = Math.floor(Math.random() * memeArray.length)
+    const url = memeArray[randomNumber].url
+    console.log(url)
+  }
+
   return (
     <main>
         <div className='form'>
@@ -14,7 +23,13 @@ const Meme = () => {
                 className='form-input'
                 placeholder='type punchline'
             />
-            <button type="submit" className='form-btn'>Get a new meme image 🖼</button>
+            <button 
+            type="submit" 
+            className='form-btn'
+            onClick={getMemeImage}
+            >
+              Get a new meme image 🖼
+            </button>
         </div>
     </main>
   )
